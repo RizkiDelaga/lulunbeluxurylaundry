e225b7d8aafc0353c260853024088f5c3c9f6853
RizkiDelaga <72425333+RizkiDelaga@users.noreply.github.com>
2023-04-12 05:48:06 +0700

    Update Progres 6.1

--- src/pages/administratorPages/businessInformationMenu/FrequentlyAskedQuestions/FrequentlyAskedQuestions.jsx
+++ src/pages/administratorPages/businessInformationMenu/FrequentlyAskedQuestions/FrequentlyAskedQuestions.jsx
@@ -1,11 +1,26 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import PageStructureAndDirectButton from '../../../../components/PageStructureAndDirectButton/PageStructureAndDirectButton';
-import { Box, Button, Grid, Paper, TextField, useTheme } from '@mui/material';
+import {
+  Box,
+  Button,
+  Grid,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  useTheme,
+} from '@mui/material';
+import axios from 'axios';
 
 function FrequentlyAskedQuestions() {
   const theme = useTheme();
   const navigate = useNavigate();
+  const [listFaq, setListFaq] = useState([]);
   const [formFrequentlyAskedQuestions, setFormFrequentlyAskedQuestions] = useState({
     question: '',
     answer: '',
@@ -13,7 +28,53 @@ function FrequentlyAskedQuestions() {
 
   React.useEffect(() => {
     document.title = 'Edit FAQ';
+    getListFaqHandler();
   }, []);
+
+  const getListFaqHandler = async () => {
+    try {
+      const res = await axios({
+        method: 'GET',
+        url: 'https://api-tugasakhir-lulu-laundry-git-develop-raihaniqbalpasya.vercel.app/api/v1/faq',
+      });
+      setListFaq(res.data.data);
+      console.log(res);
+      // console.log(listFaq);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const submitHandler = async (data) => {
+    try {
+      const res = await axios({
+        method: 'POST',
+        url: 'https://api-tugasakhir-lulu-laundry-git-develop-raihaniqbalpasya.vercel.app/api/v1/faq',
+        data: {
+          pertanyaan: data.question,
+          jawaban: data.answer,
+        },
+      });
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteFaqHandler = async (id) => {
+    try {
+      const res = await axios({
+        method: 'DELETE',
+        url: `https://api-tugasakhir-lulu-laundry-git-develop-raihaniqbalpasya.vercel.app/api/v1/faq/${id}`,
+      });
+      getListFaqHandler();
+      // setListFaq(res.data.data);
+      console.log(res);
+      // console.log(listFaq);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
@@ -90,14 +151,61 @@ function FrequentlyAskedQuestions() {
               </Grid>
             </Grid>
 
-            <Button variant="contained" size="large" style={{ width: '100%', fontWeight: 'bold' }}>
+            <Button
+              variant="contained"
+              size="large"
+              onClick={() => {
+                submitHandler(formFrequentlyAskedQuestions);
+              }}
+              style={{ width: '100%', fontWeight: 'bold' }}
+            >
               Tambah
             </Button>
+            {listFaq.length ? listFaq[0].pertanyaan : 'belum ada'}
 
             {formFrequentlyAskedQuestions.question}
             <br />
             {formFrequentlyAskedQuestions.answer}
             <br />
+            <TableContainer sx={{ maxHeight: 'none', width: '100%' }}>
+              <Table stickyHeader>
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="center" style={{ fontWeight: 'bold' }}>
+                      No
+                    </TableCell>
+                    <TableCell style={{ width: 'fit-content', fontWeight: 'bold' }}>No</TableCell>
+                    <TableCell style={{ fontWeight: 'bold' }}>column.label</TableCell>
+                    <TableCell style={{ width: 'fit-content', fontWeight: 'bold' }}></TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {listFaq.map((item, index) => {
+                    return (
+                      <TableRow hover role="checkbox" tabIndex={-1} key={item.id}>
+                        <TableCell align="center">
+                          <span>{index + 1}</span>
+                        </TableCell>
+                        <TableCell>
+                          <span>{item.pertanyaan}</span>
+                        </TableCell>
+                        <TableCell>
+                          <span>{item.jawaban}</span>
+                        </TableCell>
+                        <TableCell>
+                          <span align="right">
+                            <div>
+                              <Button>Edit</Button>
+                              <Button onClick={() => deleteFaqHandler(item.id)}>Delete</Button>
+                            </div>
+                          </span>
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </TableContainer>
           </Box>
         </Paper>
       </div>
